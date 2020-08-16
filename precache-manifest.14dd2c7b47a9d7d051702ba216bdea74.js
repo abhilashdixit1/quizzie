4b869e39b66c6dd6d13fdf1dbea60011564acd19
Nikitha Ramu <nkta.1992@gmail.com>
2020-08-16 07:07:55 +0530

    Updates

---
+++ precache-manifest.14dd2c7b47a9d7d051702ba216bdea74.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98a970676042e33ed76f7af62277ade8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "05bc88b735242a76ea9a",
+    "url": "/static/css/main.29b166e4.chunk.css"
+  },
+  {
+    "revision": "499dd5919112591085b2",
+    "url": "/static/js/2.29596f8b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.29596f8b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05bc88b735242a76ea9a",
+    "url": "/static/js/main.760da7be.chunk.js"
+  },
+  {
+    "revision": "02b44a43c56ff71298ea",
+    "url": "/static/js/runtime-main.a9462892.js"
+  }
+]);
